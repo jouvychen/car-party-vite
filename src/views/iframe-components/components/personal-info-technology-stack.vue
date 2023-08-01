76f9f462a10ccc9844c2523e4cfde69d433d81bb
jouvychen <jouvy1755@163.com>
2023-08-01 18:25:05 +0800

    style: 调整个人信息样式

--- src/views/iframe-components/components/personal-info-technology-stack.vue
+++ src/views/iframe-components/components/personal-info-technology-stack.vue
@@ -11,10 +11,31 @@
     <div class="account-title f-18">Frontend Developer</div>
   </div>
   <!-- 联系方式 -->
-  <div class="card">
-    <div class="account-cash">$ 5637.04</div>
-    <div class="account-income">Total Income</div>
-    <div class="account-iban">**** **** **** 3060</div>
+  <div class="card f-18">
+    <div class="account-cash flex-center">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+        stroke-linecap="round" stroke-linejoin="round" class="account-svg">
+        <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z" />
+        <circle cx="12" cy="10" r="3" />
+      </svg>
+      <span>广东广州</span>
+    </div>
+    <div class="account-income flex-center">
+      <svg class="account-svg" viewBox="0 0 512 512" fill="currentColor">
+        <path
+          d="M467 76H45a45 45 0 00-45 45v270a45 45 0 0045 45h422a45 45 0 0045-45V121a45 45 0 00-45-45zm-6.3 30L287.8 278a44.7 44.7 0 01-63.6 0L51.3 106h409.4zM30 384.9V127l129.6 129L30 384.9zM51.3 406L181 277.2l22 22c14.2 14.1 33 22 53.1 22 20 0 38.9-7.9 53-22l22-22L460.8 406H51.3zM482 384.9L352.4 256 482 127V385z">
+        </path>
+      </svg>
+      <span>jouvy1755@163.com</span>
+    </div>
+    <div class="account-income flex-center">
+      <svg class="account-svg" viewBox="0 0 512 512" fill="currentColor">
+        <path
+          d="M467 76H45a45 45 0 00-45 45v270a45 45 0 0045 45h422a45 45 0 0045-45V121a45 45 0 00-45-45zm-6.3 30L287.8 278a44.7 44.7 0 01-63.6 0L51.3 106h409.4zM30 384.9V127l129.6 129L30 384.9zM51.3 406L181 277.2l22 22c14.2 14.1 33 22 53.1 22 20 0 38.9-7.9 53-22l22-22L460.8 406H51.3zM482 384.9L352.4 256 482 127V385z">
+        </path>
+      </svg>
+      <span>jouvychen@gmail.com</span>
+    </div>
   </div>
 </template>
 
@@ -98,19 +119,17 @@ const avatar = getAssetsUrlRelative('../assets/images/iframe-page/about-me/', 'a
 
   // 联系方式
   &-cash {
-    font-size: 22px;
-    font-weight: 500;
     margin-bottom: 6px;
     position: relative;
   }
 
   &-income {
-    font-size: 14px;
+    font-size: 16px;
   }
 
-  &-iban {
-    margin-top: auto;
-    font-weight: 500;
+  &-svg {
+    width: 16px;
+    margin-right: 6px;
   }
 }
 
